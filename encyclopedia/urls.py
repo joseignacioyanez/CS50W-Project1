8ee4ca762fe245affe02be1b03a03e671ea0dbe0
José Ignacio Yánez <joseignacioyanez@hotmail.com>
2022-12-28 02:56:47 -0500

    Finished all 7 Requirements

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("wiki", views.index, name="index"),
+    path("wiki/search", views.search, name="search"),
+    path("wiki/new", views.newPage, name="newPage"),
+    path("wiki/random", views.random, name="random"),
+    path("wiki/edit/<str:entryName>", views.editPage, name="editPage"),
     path("wiki/<str:entryName>", views.entryShow, name="entryShow")
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.files import File
+
+from random import randrange
 
 from markdown2 import Markdown
 
@@ -6,8 +9,6 @@ from . import util
 
 
 def index(request):
-    
-
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
     })
@@ -28,3 +29,70 @@ def entryShow(request, entryName):
             "entryName":entryName,
             "entryHTML":entryHTML
         })
+
+def search(request):
+    # Check if request has parameters
+    query = request.GET.get('q')
+    if not query:
+        return render(request, "encyclopedia/search.html", {
+            "noQuery":True
+        })    
+    
+    entries = [x for x in util.list_entries()]
+
+    # Exact match
+    if query in entries:
+        return redirect("entryShow", entryName=query)
+    else:
+        matches = [entry for entry in entries if f"{query.lower()}" in entry.lower()]
+
+        return render(request, "encyclopedia/search.html", {
+            "matches":matches
+        })
+
+def newPage(request):
+    if request.method == "POST":
+        # Check if name is OK
+        try:
+            newEntryName = request.POST.get('entryTitle').lower()
+            entries = [x.lower() for x in util.list_entries()]
+        except Exception as e:
+            return render(request, "encyclopedia/error.html")
+        
+        if newEntryName in entries:
+            return render(request, "encyclopedia/newPage.html", {
+                "entryMarkdown": request.POST.get('entryMarkdown'),
+                "error":True
+            })
+        # Save and Redirect 
+        util.save_entry(request.POST.get('entryTitle'), request.POST.get('entryMarkdown'))
+        
+        return redirect("entryShow", entryName=newEntryName)
+    else:
+        return render(request, "encyclopedia/newPage.html")
+
+def editPage(request, entryName):
+    if request.method == "POST":
+        # Save and Redirect 
+        util.save_entry(request.POST.get('entryTitle'), request.POST.get('entryMarkdown'))
+        
+        return redirect("entryShow", entryName=request.POST.get('entryTitle'))
+    else:
+        entrySearched = util.get_entry(entryName)
+        if not entrySearched:
+            return render(request, "encyclopedia/error.html")
+
+        entryMarkdown = util.get_entry(entryName)
+        entryTitle = entryName
+        return render(request, "encyclopedia/editPage.html", {
+            "entryTitle" : entryTitle,
+            "entryMarkdown": entryMarkdown
+        })
+
+def random(request):
+
+    entries = util.list_entries()
+
+    randomIndex = randrange(len(entries)-1)
+
+    return redirect("entryShow", entryName=entries[randomIndex])
